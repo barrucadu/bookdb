e19a13de41f99845234b90976f94e26fd072b56b
Michael Walker <mike@barrucadu.co.uk>
2014-06-03 00:24:06 +0100

    Implement (except for cover uploading) book adding and editing

--- src/Handler/Edit.hs
+++ src/Handler/Edit.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Handler.Edit
     ( -- * Display forms
       add
@@ -10,12 +12,18 @@ module Handler.Edit
     , commitDelete
     ) where
 
-import Control.Applicative ((<$>))
-import Data.Text (Text)
+import Prelude hiding (null, userError)
+
+import Data.List (sort)
+import Data.Text (Text, null, intercalate, splitOn, unpack, isInfixOf)
+import Data.Time.Calendar (fromGregorian)
+import Data.Time.Clock (UTCTime(..))
 import Database
-import Database.Persist (Entity(..))
+import Database.Persist (Entity(..), insert, replace)
+import Handler.Information
 import Handler.Utils
 import Routes
+import Text.Read (readMaybe)
 import Web.Seacat
 import Web.Seacat.RequestHandler (htmlUrlResponse)
 
@@ -67,10 +75,71 @@ delete' (Entity bookId book) = htmlUrlResponse $ T.confirmDelete book
 -------------------------
 
 commitAdd' :: Handler Sitemap
-commitAdd' = undefined
+commitAdd' = mutate Nothing
 
 commitEdit' :: Entity Book -> Handler Sitemap
-commitEdit' (Entity bookId book) = undefined
+commitEdit' = mutate . Just
 
 commitDelete' :: Entity Book -> Handler Sitemap
 commitDelete' (Entity bookId book) = undefined
+
+-------------------------
+
+-- |Mutate a book, and display a notification when done.
+mutate :: Maybe (Entity Book) -- ^ The book to mutate, or Nothing to insert
+       -> Handler Sitemap
+mutate book = do
+  -- do cover upload
+  let cover = undefined
+
+  isbn       <- param' "isbn"       ""
+  title      <- param' "title"      ""
+  subtitle   <- param' "subtitle"   ""
+  volume     <- param' "volume"     ""
+  fascicle   <- param' "fascicle"   ""
+  voltitle   <- param' "voltitle"   ""
+  author     <- param' "author"     ""
+  translator <- param' "translator" ""
+  editor     <- param' "editor"     ""
+  read       <- param' "read"       ""
+  lastread   <- param' "lastread"   ""
+  location   <- param' "location"   ""
+  borrower   <- param' "borrower"   ""
+  quote      <- param' "quote"      ""
+  notes      <- param' "notes"      ""
+
+  if null isbn || null title || null author || null location
+  then userError "Missing required fields"
+  else do
+    let author'     = sortAuthors author
+    let translator' = empty translator
+    let editor'     = empty editor
+    let read'       = set read
+
+    case toDate lastread of
+      Just lastread' -> do
+        let newbook = Book cover isbn title subtitle volume fascicle voltitle author' translator' editor' read' lastread' location borrower quote notes
+
+        case book of
+          Just (Entity bookId _) -> replace bookId newbook >> information "Book updated successfully"
+          Nothing -> insert newbook >> information "Book added successfully"
+
+      Nothing -> userError "Invalid date format, expected yyyy-mm-dd"
+
+  where sortAuthors = intercalate " & " . sort . splitOn " & "
+
+        empty "" = Nothing
+        empty t  = Just t
+
+        set "" = False
+        set _  = True
+
+        toDate t = if null t
+                   then Just Nothing
+                   else case toDay t of
+                          Just d  -> Just $ Just UTCTime { utctDay = d, utctDayTime = 0 }
+                          Nothing -> Nothing
+
+        toDay t = case map (readMaybe . unpack) $ splitOn "-" t of
+                    (Just y : Just m : Just d : []) -> Just $ fromGregorian (fromIntegral y) m d
+                    _          -> Nothing
