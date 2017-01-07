34d9da77377459e912fc3139f647212106db47ae
Michael Walker <mike@barrucadu.co.uk>
2017-01-07 04:13:48 +0000

    Remove caption from cover hovers

--- static/script.js
+++ static/script.js
@@ -4,17 +4,13 @@ $(document).ready(function(){
 	yOffset = 30;
 
 	$("a.preview").hover(function(e){
-		this.t = this.title;
-		this.title = "";
-		var c = (this.t != "") ? "<br/>" + this.t : "";
-		$("body").append("<p id='preview'><img src='"+ this.href +"' alt='Image preview' />"+ c +"</p>");
+		$("body").append("<p id=\"preview\"><img src=\"" + this.href + "\" alt=\"" + this.title + "\" /></p>");
 		$("#preview")
 			.css("top",(e.pageY - xOffset) + "px")
 			.css("left",(e.pageX + yOffset) + "px")
 			.fadeIn("fast");
     },
 	function(){
-		this.title = this.t;
 		$("#preview").remove();
     });
 
