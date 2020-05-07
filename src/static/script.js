81f1d57c118c7a42b56b86e2ec9c760016d25dff
Michael Walker <mike@barrucadu.co.uk>
2020-05-07 22:29:21 +0100

    Implement "+" buttons for people and holdings on edit form

--- src/static/script.js
+++ src/static/script.js
@@ -122,3 +122,68 @@ function autocomplete(inp, arr) {
       closeAllLists(e.target);
   });
 }
+
+function add_person_entry(container_clazz, name, entries) {
+    let container = document.getElementsByClassName(container_clazz)[0];
+    let position = container.getElementsByClassName("js-insert-before")[0];
+    let el = document.createElement("div");
+    let input = document.createElement("input");
+    el.setAttribute("class", "autocomplete");
+    input.setAttribute("class", "form-input");
+    input.setAttribute("type", "text");
+    input.setAttribute("name", name);
+    el.appendChild(input);
+    container.insertBefore(el, position);
+    container.insertBefore(document.createTextNode(" "), position);
+    autocomplete(input, entries);
+}
+
+var location_count = 0;
+
+function add_holding_entry(n, locations) {
+    location_count += 1;
+    let nth = n+location_count;
+
+    let container = document.getElementsByClassName("js-holdings")[0];
+    let position = container.getElementsByClassName("js-insert-before")[0];
+    let row = document.createElement("div");
+    row.setAttribute("class", "form-row");
+    let group_location = document.createElement("div");
+    group_location.setAttribute("class", "form-group");
+    let group_notes = document.createElement("div");
+    group_notes.setAttribute("class", "form-group");
+    row.appendChild(group_location);
+    row.appendChild(group_notes);
+    let label_location = document.createElement("location");
+    label_location.setAttribute("class", "form-label");
+    label_location.setAttribute("for", `location_${nth}`);
+    label_location.innerText = `Location ${nth}`;
+    group_location.appendChild(label_location);
+    let label_notes = document.createElement("location");
+    label_notes.setAttribute("class", "form-label");
+    label_notes.setAttribute("for", `notes_${nth}`);
+    label_notes.innerText = "Notes";
+    group_notes.appendChild(label_notes);
+    let input_location = document.createElement("select");
+    input_location.setAttribute("class", "form-input form-input-narrow");
+    input_location.setAttribute("id", `location_${nth}`);
+    input_location.setAttribute("name", "location[]");
+    let option = document.createElement("option");
+    option.value = "";
+    option.selected = "";
+    input_location.add(option);
+    locations.forEach(function(loc) {
+        let option = document.createElement("option");
+        option.value = loc.key;
+        option.text = loc.label;
+        input_location.add(option);
+    });
+    group_location.appendChild(input_location);
+    let input_notes = document.createElement("input");
+    input_notes.setAttribute("class", "form-input");
+    input_notes.setAttribute("id", `notes_${nth}`);
+    input_notes.setAttribute("name", "notes[]");
+    input_notes.setAttribute("type", "text");
+    group_notes.appendChild(input_notes);
+    container.insertBefore(row, position);
+}
