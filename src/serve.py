f0db42643803c6f0401506cf41b6b9f9187959e1
Michael Walker <mike@barrucadu.co.uk>
2020-05-07 18:19:47 +0100

    Implement HTML search page

--- src/serve.py
+++ src/serve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, abort, jsonify, request, send_from_directory
+from flask import Flask, abort, jsonify, redirect, render_template, request, send_from_directory
 
 from elasticsearch import Elasticsearch
 from elasticsearch.exceptions import NotFoundError
@@ -11,31 +11,39 @@ app = Flask(__name__)
 
 es = Elasticsearch([os.getenv("ES_HOST", "http://localhost:9200")])
 
+BASE_URI = os.getenv("BASE_URI", "http://bookdb.nyarlathotep")
+
 COVER_DIR = os.getenv("COVER_DIR", "covers")
 
-CATEGORIES = {
-    "590ac55d-0644-4a71-b902-587faa5b03d9": ("Prose Fiction + Visual Novels", None),
-    "c2bfb2d8-ce64-4480-aaa6-c2d2dcb34ace": ("Gothic, Horror, + Weird", "590ac55d-0644-4a71-b902-587faa5b03d9",),
-    "bcb54905-2e45-4a8f-bda0-e1d12e4114d2": ("High Fantasy", "590ac55d-0644-4a71-b902-587faa5b03d9",),
-    "ea4e9b04-3ebb-4ff3-b387-d8bfc603f58a": ("Miscellaneous", "590ac55d-0644-4a71-b902-587faa5b03d9",),
-    "a3bbb1c6-5ff8-4ddf-81f4-820593a2a5ff": ("Manga", None),
-    "217f8eaa-b54a-466c-83f8-3f569f46732e": ("Verse", None),
-    "a7d83bc9-0352-4fb5-a9dc-68428562a17f": ("Religion, Mythology, + Folklore", None),
-    "384664d5-e256-4787-a729-1eb5e7a7de6f": ("Abrahamic", "a7d83bc9-0352-4fb5-a9dc-68428562a17f",),
-    "0435b803-d2e9-49b9-a1c7-bc7fc1b30cdd": ("Miscellaneous", "a7d83bc9-0352-4fb5-a9dc-68428562a17f",),
-    "58c63aea-72b6-4988-96a0-aca572661303": ("Nonfiction", None),
-    "219ae9f4-84f1-4ea2-a9cd-fbc16698a669": ("RPG Rulebooks", "58c63aea-72b6-4988-96a0-aca572661303",),
-    "afc7135b-bf67-4284-bcc1-2bbd3386aea3": ("Computer Science, Software Engineering, + Digital Culture", "58c63aea-72b6-4988-96a0-aca572661303",),
-    "ac4706f3-54c3-4e6d-a72d-85321d9dcd72": ("Politics, Philosophy, Economics, + History", "58c63aea-72b6-4988-96a0-aca572661303",),
-    "b5451ba6-a3ef-4c27-99fc-a07d8cd54161": ("Kitchen, Food, + Recipe", "58c63aea-72b6-4988-96a0-aca572661303",),
-    "fb2dd601-883a-4254-a294-fcf0a0f98d2f": ("Miscellaneous", "58c63aea-72b6-4988-96a0-aca572661303",),
-}
+ALLOW_WRITES = os.getenv("ALLOW_WRITES", "0") == "1"
 
-LOCATIONS = {
-    "be60be7b-a10f-42e1-8769-d43f12cad02d": "London",
-    "f256ed66-4c09-4207-86de-adc8e9fb86ec": "Hull",
-    "6a233e5e-3b64-4169-ac67-cf46113afd97": "Missing",
-}
+ORDERED_CATEGORIES = [
+    ("590ac55d-0644-4a71-b902-587faa5b03d9", ("Prose Fiction + Visual Novels", None)),
+    ("c2bfb2d8-ce64-4480-aaa6-c2d2dcb34ace", ("Gothic, Horror, + Weird", "590ac55d-0644-4a71-b902-587faa5b03d9")),
+    ("bcb54905-2e45-4a8f-bda0-e1d12e4114d2", ("High Fantasy", "590ac55d-0644-4a71-b902-587faa5b03d9")),
+    ("ea4e9b04-3ebb-4ff3-b387-d8bfc603f58a", ("Miscellaneous", "590ac55d-0644-4a71-b902-587faa5b03d9")),
+    ("a3bbb1c6-5ff8-4ddf-81f4-820593a2a5ff", ("Manga", None)),
+    ("217f8eaa-b54a-466c-83f8-3f569f46732e", ("Verse", None)),
+    ("a7d83bc9-0352-4fb5-a9dc-68428562a17f", ("Religion, Mythology, + Folklore", None)),
+    ("384664d5-e256-4787-a729-1eb5e7a7de6f", ("Abrahamic", "a7d83bc9-0352-4fb5-a9dc-68428562a17f")),
+    ("0435b803-d2e9-49b9-a1c7-bc7fc1b30cdd", ("Miscellaneous", "a7d83bc9-0352-4fb5-a9dc-68428562a17f")),
+    ("58c63aea-72b6-4988-96a0-aca572661303", ("Nonfiction", None)),
+    ("219ae9f4-84f1-4ea2-a9cd-fbc16698a669", ("RPG Rulebooks", "58c63aea-72b6-4988-96a0-aca572661303")),
+    ("afc7135b-bf67-4284-bcc1-2bbd3386aea3", ("Computer Science, Software Engineering, + Digital Culture", "58c63aea-72b6-4988-96a0-aca572661303")),
+    ("ac4706f3-54c3-4e6d-a72d-85321d9dcd72", ("Politics, Philosophy, Economics, + History", "58c63aea-72b6-4988-96a0-aca572661303")),
+    ("b5451ba6-a3ef-4c27-99fc-a07d8cd54161", ("Kitchen, Food, + Recipe", "58c63aea-72b6-4988-96a0-aca572661303")),
+    ("fb2dd601-883a-4254-a294-fcf0a0f98d2f", ("Miscellaneous", "58c63aea-72b6-4988-96a0-aca572661303")),
+]
+
+CATEGORIES = {k: v for k, v in ORDERED_CATEGORIES}
+
+ORDERED_LOCATIONS = [
+    ("be60be7b-a10f-42e1-8769-d43f12cad02d", "London"),
+    ("f256ed66-4c09-4207-86de-adc8e9fb86ec", "Hull"),
+    ("6a233e5e-3b64-4169-ac67-cf46113afd97", "Missing"),
+]
+
+LOCATIONS = {k: v for k, v in ORDERED_LOCATIONS}
 
 
 def __expand_category(uuid):
@@ -55,12 +63,18 @@ def __children_categories(uuid):
     return out
 
 
+def __full_category_name(uuid):
+    return " / ".join(reversed(__expand_category(uuid)))
+
+
 MEMOISED_EXPAND_CATEGORY = {}
 MEMOISED_CHILDREN_CATEGORIES = {}
+MEMOISED_FULL_CATEGORY_NAME = {}
 
 for uuid in CATEGORIES.keys():
     MEMOISED_EXPAND_CATEGORY[uuid] = __expand_category(uuid)
     MEMOISED_CHILDREN_CATEGORIES[uuid] = __children_categories(uuid)
+    MEMOISED_FULL_CATEGORY_NAME[uuid] = __full_category_name(uuid)
 
 
 def expand_category(uuid):
@@ -69,6 +83,10 @@ def expand_category(uuid):
 
 def children_categories(uuid):
     return MEMOISED_CHILDREN_CATEGORIES[uuid]
+
+
+def full_category_name(uuid):
+    return MEMOISED_FULL_CATEGORY_NAME[uuid]
 
 
 def expand_location(uuid):
@@ -83,6 +101,7 @@ def transform_book(bId, book):
             return bit
 
     book["id"] = bId
+    book["has_cover_image"] = "cover_image_mimetype" in book
 
     if "volume_number" in book:
         book["volume_number"]["bits"] = [convert_bit(bit) for bit in book["volume_number"]["bits"]]
@@ -93,6 +112,7 @@ def transform_book(bId, book):
             holding["location"] = expand_location(holding["location_uuid"])
     if "category_uuid" in book:
         book["category"] = expand_category(book["category_uuid"])
+        book["category_name"] = full_category_name(book["category_uuid"])
 
     return book
 
@@ -120,16 +140,19 @@ def do_search(request_args):
     search_params = {}
     for k in request_args:
         if k in ["keywords", "match", "location", "category"]:
-            search_params[k] = request_args.get(k)
+            v = request_args.get(k)
+            if v:
+                search_params[k] = v
         elif k in ["author[]", "editor[]", "translator[]"]:
-            search_params[k] = request_args.getlist(k)
+            vs = [v for v in request_args.getlist(k) if v]
+            if vs:
+                search_params[k] = vs
 
     filters = {
         "people.authors": search_params.get("author[]"),
         "people.editors": search_params.get("editor[]"),
         "people.translators": search_params.get("translator[]"),
-        "has_been_read": {"only-read": True, "only-unread": False}.get(search_params.get("match"), None),
-        "holdings.location_uuid": search_params.get("location"),
+        "has_been_read": {"only-read": [True], "only-unread": [False]}.get(search_params.get("match"), None),
     }
 
     queries = []
@@ -140,6 +163,8 @@ def do_search(request_args):
     for f, vs in filters.items():
         for v in vs or []:
             queries.append({"term": {f: v}})
+    if "location" in search_params:
+        queries.append({"nested": {"path": "holdings", "query": {"bool": {"must": {"term": {"holdings.location_uuid": search_params["location"]}}}}}})
     if "category" in search_params:
         queries.append({"terms": {"category_uuid": children_categories(search_params["category"])}})
 
@@ -175,6 +200,51 @@ def do_search(request_args):
         "count": len(hits),
         "search_params": search_params,
     }
+
+
+def standard_template_args():
+    all_books = do_search({})
+
+    return {
+        "allow_writes": ALLOW_WRITES,
+        "base_uri": BASE_URI,
+        "locations": [{"key": k, "label": v} for k, v in ORDERED_LOCATIONS],
+        "categories": [{"key": k, "label": full_category_name(k)} for k, _ in ORDERED_CATEGORIES],
+        "authors": [d["key"] for d in all_books["aggregations"]["author"]],
+        "editors": [d["key"] for d in all_books["aggregations"]["editor"]],
+        "translators": [d["key"] for d in all_books["aggregations"]["translator"]],
+    }
+
+
+@app.route("/")
+def redirect_index():
+    return redirect(f"{BASE_URI}/search", code=301)
+
+
+@app.route("/list")
+def redirect_list():
+    return redirect(f"{BASE_URI}/search", code=301)
+
+
+@app.route("/search")
+def search():
+    results = do_search(request.args)
+
+    num_read = list(d["doc_count"] for d in results["aggregations"]["match"] if d["key"] == "only-read")
+    if num_read:
+        num_read = min(num_read)
+    else:
+        num_read = 0
+
+    return render_template(
+        "search.html",
+        books=results["books"],
+        num_authors=len(results["aggregations"]["author"]),
+        num_read=num_read,
+        percent_read=int((num_read / results["count"]) * 100) if results["count"] > 0 else 100,
+        search_params=results["search_params"],
+        **standard_template_args(),
+    )
 
 
 @app.route("/search.json")
